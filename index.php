ae68b79a55cbefdaadd99f2864579dbc5b2a6575
Przemyslaw Tarchala <przemyslaw.tarchala.programista@gmail.com>
2024-02-20 13:24:27 +0100

    Add DB to app, create Database class

--- index.php
+++ index.php
@@ -7,12 +7,17 @@ namespace App;
 require_once("src/Utils/debug.php");
 require_once("src/Controller.php");
 
+$configuration = require_once("config/config.php"); 
+
 $request = [
 	'get' => $_GET,
 	'post' => $_POST
 ];
 
+Controller::initConfiguration($configuration);
 (new Controller($request))->run();
+
+
 
 // $controller = new Controller($request);
 // $controller->run();
---
+++ src/Database.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+ 
+use Exception;
+use PDO;
+
+class Database
+{
+	public function __construct(array $config)
+	{
+		$dsn = "mysql:dbname={$config['database']};host={$config['host']}";
+		
+		try {
+			$connection = new PDO(
+				$dsn,
+				$config['user'],
+				$config['password']
+			);
+		} catch (Exception $error) {
+			dump($error);
+		}
+		// dump($connection);
+	}
+}
